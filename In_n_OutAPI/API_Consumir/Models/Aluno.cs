c50d7f5617bcc59503a69f163928deba136a5ceb
flaviarosadolima <44933833+flaviarosadolima@users.noreply.github.com>
2020-04-29 19:51:38 -0300

    Add files via upload

---
+++ In_n_OutAPI/API_Consumir/Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Consumir.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = "";
+        public string CEP { get; set; } = "";
+    }
+}
